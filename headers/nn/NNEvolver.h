4014095ea06a88ea83563f303cebad7844b2f7af
nanoandrew4 <nanoandrew4@gmail.com>
2018-03-24 14:00:30 -0300

    Added more documentation

--- headers/nn/NNEvolver.h
+++ headers/nn/NNEvolver.h
@@ -18,10 +18,44 @@ private:
 
     static bool threadReady[threads];
 
-    void train(AIPlayer*[]);
+    /**
+     * Takes care of initializing the worker threads to simulate a tournament, and after all threads are done,
+     * the best agents are combined to make the theoretical best agent. All players are reset to be a noisy version
+     * of the best agent
+     *
+     * @param players Array of AIPlayers to be trained
+     */
+    void train(AIPlayer* players[]);
+
+    /**
+     * Training thread, simulates 'itersPerGen' tables, using players in array 'players' between 'startPlayer' index and
+     * 'endPlayer' index.
+     *
+     * @param players Array of AIPlayers to be trained
+     * @param playersPerTable Number of players that can sit at a table
+     * @param threadNum Number of the thread, in order to coordinate between threads using threadReady array
+     * @param startPlayer Index of players array this thread should start at
+     * @param endPlayer Index of players array this thread should end at
+     */
     void trainThread(AIPlayer* players[], int playersPerTable, int threadNum, int startPlayer, int endPlayer);
+
+    /**
+     * Blocks thread until all threads have set their index in threadReady array to true
+     */
     void blockUntilReady();
+
+    /**
+     * Sorts array of agents in descending order, based on how much money they have
+     */
     void quicksort(AIPlayer*[], int, int);
+
+    /**
+     * Combines the parent players to generate the next generation agent. All other players copy that agent and apply
+     * some random noise to it, in order to keep evolution from stagnating.
+     *
+     * @param players Array containing all agents
+     * @param parents Array containing agents to be combined for the next generation
+     */
     void generateNextGen(AIPlayer* players[], AIPlayer* parents[]);
 };
 
